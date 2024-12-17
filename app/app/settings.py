ecc722ca3723732736a5686c8a1032a1b0f3fc21
HONOR <busyginz@mail.ru>
2024-12-17 08:29:46 +0700

    static

--- app/app/settings.py
+++ app/app/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [BASE_DIR / 'static',
+STATICFILES_DIRS = [ '/home/user/PycharmProjects/Magazine_django/static',
                     ]
 
 # Default primary key field type
