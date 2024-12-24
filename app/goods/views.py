8249be8cb9e9152b273b9cd64b453bca932c0237
Yugenyi Busygin <abusyginza@gmail.com>
2024-12-24 16:43:58 +0700

    постраничный переход товаров

--- app/goods/views.py
+++ app/goods/views.py
@@ -1,10 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from goods.models import Products
 
 
-def catalog(request):
+def catalog(request, category_slug):
 
-    goods = Products.objects.all()
+    if category_slug == 'all':
+        goods = Products.objects.all()
+    else:
+        goods = get_object_or_404(Products.objects.filter(category__slug=category_slug))
+        
+
+    
     context = {
         "title": "Home - Каталог",
         "goods": goods,
@@ -13,5 +19,12 @@ def catalog(request):
     return render(request, "goods/catalog.html", context)
 
 
-def product(request):
-    return render(request, "goods/product.html")
+def product(request, product_slug):
+    product = Products.objects.get(slug=product_slug)
+    context = {
+        'product': product,
+    }
+    return render(request, "goods/product.html", context=context)
+
+
+
