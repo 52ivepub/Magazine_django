bb24f4de329d80e32ab9521d36fa8a9f01c45b63
Yugenyi Busygin <abusyginza@gmail.com>
2025-01-02 16:18:49 +0700

    forms login

--- app/users/forms.py
+++ app/users/forms.py
@@ -1,7 +1,20 @@
 from django import forms 
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UsernameField
 from users.models import Users
 
 class UserLoginForm(AuthenticationForm):
+    
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={"autofocus": True,
+                                      'class': 'form-control',
+                                      'placeholder': 'Введите имя пользователя'
+                                                             }))
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={"autocomplete": "current-password", 
+                                        'class': 'form-control',
+                                        'placeholder': 'Введите пароль'
+                                          }),
+    )
     class Meta:
         model = Users
+        fields = ['username','password']
--- app/users/views.py
+++ app/users/views.py
@@ -1,10 +1,24 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
-
+from django.contrib import auth
 from users.forms import UserLoginForm
+from django.urls import reverse
 
 
 def login(request):
-    form = UserLoginForm()
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(usermane=username, password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('main:index'))
+    else:
+        form = UserLoginForm()
+            
+
     context = {
         'title': 'Home - Авторизация',
         'form': form,
