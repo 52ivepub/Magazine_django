5cff009f43577efe2345493eea8a595241eb2090
Yugenyi Busygin <abusyginza@gmail.com>
2025-01-03 17:47:11 +0700

    login_form

--- app/users/forms.py
+++ app/users/forms.py
@@ -5,11 +5,13 @@ from users.models import Users
 class UserLoginForm(AuthenticationForm):
     
     username = forms.CharField(
+        label = 'Имя',
         widget=forms.TextInput(attrs={"autofocus": True,
                                       'class': 'form-control',
                                       'placeholder': 'Введите имя пользователя'
                                                              }))
     password = forms.CharField(
+        label = 'Пароль',
         widget=forms.PasswordInput(attrs={"autocomplete": "current-password", 
                                         'class': 'form-control',
                                         'placeholder': 'Введите пароль'
